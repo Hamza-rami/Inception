9b3dbcc3acafdfe8c1e1d3a5ae6cb7cb0a6ad95b
Hamza Rami <ramihamza504@gmail.com>
2026-07-03 12:31:45 +0100

    finished script maraidb and start setup contaianer wordpress

---
+++ srcs/requirements/wordpress/script.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+WP_PATH="/var/www/html"
+
+until mysqladmin \
+    --host="$WORDPRESS_DB_HOST" \
+    --user="$MYSQL_USER" \
+    --password="$MYSQL_PASSWORD" \
+    ping --silent
+do
+    sleep 1
+done
+
+
+if [ ! -f "$WP_PATH/wp-load.php" ]; then
+    wp core download --path="$WP_PATH"
+fi
+
+
+if [ ! -f "$WP_PATH/wp-config.php" ]; then
+    wp config create \
+        --path="$WP_PATH" \
+        --dbname="$MYSQL_DATABASE" \
+        --dbuser="$MYSQL_USER" \
+        --dbpass="$MYSQL_PASSWORD" \
+        --dbhost="$WORDPRESS_DB_HOST"
+        --skip-check
+fi
+
+if ! su -s /bin/bash www-data -c \
+    "wp core is-installed --path='$WP_PATH'"; then
+
+    su -s /bin/bash www-data -c "
+        wp core install \
+            --path='$WP_PATH' \
+            --url='$DOMAIN_NAME' \
+            --title='$WP_TITLE' \
+            --admin_user='$WP_ADMIN' \
+            --admin_password='$WP_ADMIN_PASSWORD' \
+            --admin_email='$WP_ADMIN_EMAIL' \
+            --skip-email
+    "
+fi
