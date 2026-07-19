2194f37ff4958100e102539c09bcc74afd68d458
Hamza Rami <ramihamza504@gmail.com>
2026-07-19 16:49:00 +0100

    feat: finnished scripts and dockerfils for all contianers && docker compose

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -4,6 +4,39 @@ set -e
 
 WP_PATH="/var/www/html"
 
-if [ ! -f /var/www/html/wp-config.php ]; then
+until mysqladmin ping -h mariadb --silent; do
+    sleep 1
+done
+
+if [ ! -f "$WP_PATH/wp-config.php" ]; then
+
+    wp core download --path="$WP_PATH" --allow-root
+
+    wp config create \
+        --dbname="${MYSQL_DATABASE}" \
+        --dbuser="${MYSQL_USER}" \
+        --dbpass="${MYSQL_PASSWORD}" \
+        --dbhost="mariadb" \
+        --path="${WP_PATH}" \
+        --allow-root
+    
+    wp core install \
+        --url="${DOMAIN_NAME}" \
+        --title="${WP_TITLE}" \
+        --admin_user="${WP_ADMIN}" \
+        --admin_password="${WP_ADMIN_PASSWORD}" \
+        --admin_email="${WP_ADMIN_EMAIL}" \
+        --path="$WP_PATH" \
+        --allow-root
+
+    wp user create \
+        "${WP_USER}" \
+        "${WP_USER_EMAIL}" \
+        --user_pass="${WP_USER_PASSWORD}" \
+        --role=author \
+        --path="$WP_PATH" \
+        --allow-root
 
 fi
+
+exec php-fpm -F
